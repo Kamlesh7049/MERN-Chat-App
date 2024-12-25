d7bc1ebf70487e5a9ee6def250e09411b75cda42
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2024-12-25 20:51:19 +0530

    first

---
+++ app.js
@@ -0,0 +1,14 @@
+// Code to create a simple express server
+   import express from 'express';
+
+   const app = express();
+
+   app.use(express.json());
+   app.use(express.urlencoded({ extended: true }));
+
+
+   app.get('/', (req, res) => {
+     res.send('Hello World');
+   });
+
+   export default app;
